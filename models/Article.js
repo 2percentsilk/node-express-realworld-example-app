30a62eaa871b1a31357ca38a6986a57ab6436a41
Jonathan Carter <joncart@outlook.com>
2018-02-05 11:43:01 -0800

    Introducing demo issue

--- models/Article.js
+++ models/Article.js
@@ -28,7 +28,7 @@ ArticleSchema.methods.slugify = function() {
   this.slug = slug(this.title) + '-' + (Math.random() * Math.pow(36, 6) | 0).toString(36);
 };
 
-ArticleSchema.methods.updateFavoriteCount = async function () {
+ArticleSchema.methods.updateFavoriteCount = async () => {
   this.favoritesCount = await User.count({
     favorites: { $in: [this._id] }
   });
